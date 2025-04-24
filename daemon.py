d18262ba6dee28cd92c9397da790e9fcd9c6122f
ramilnurgaleev <nurgaleev.dev.tat@gmail.com>
2025-04-24 12:11:01 +0300

    initial

---
+++ daemon.py
@@ -0,0 +1,36 @@
+import time
+import schedule
+from datetime import datetime
+from importlib.machinery import SourceFileLoader
+
+# Загружаем модуль main.py как отдельный модуль
+main_module = SourceFileLoader("main_module", "/home/avito_add_image/main.py").load_module()
+download_xml = main_module.download_xml
+process_xml_with_gdrive = main_module.process_xml_with_gdrive
+
+def job():
+    """Основная функция для запуска процесса обработки"""
+    print(f"Начало обработки: {datetime.now()}")
+    
+    if download_xml():
+        df, file_url = process_xml_with_gdrive()
+        print(f"Ссылка на обработанный документ: {file_url}")
+    print(f"Обработка завершена: {datetime.now()}")
+
+def main():
+    """Основная функция демона"""
+    print("Демон запущен...")
+    
+    # Запускаем задачу сразу при старте
+    job()
+    
+    # Планируем выполнение каждые 5 минут
+    schedule.every(5).minutes.do(job)
+    
+    # Бесконечный цикл для выполнения запланированных задач
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+if __name__ == "__main__":
+    main() 
